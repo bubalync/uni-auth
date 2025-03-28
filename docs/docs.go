410fe03d2d26dd2341d39801c4bf02aa149aba55
flor3sc0 <61797396+flor3sc0@users.noreply.github.com>
2025-03-28 19:12:55 +0700

    update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -15,9 +15,9 @@ const docTemplate = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
-        "/api/ping": {
-            "get": {
-                "description": "do ping",
+        "/api/v1/users/register": {
+            "post": {
+                "description": "Register a new user",
                 "consumes": [
                     "application/json"
                 ],
@@ -25,16 +25,82 @@ const docTemplate = `{
                     "application/json"
                 ],
                 "tags": [
-                    "example"
+                    "Users"
                 ],
-                "summary": "ping example",
+                "summary": "Register",
+                "operationId": "do-translate",
+                "parameters": [
+                    {
+                        "description": "Registration payload",
+                        "name": "request",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/v1.registerRequest"
+                        }
+                    }
+                ],
                 "responses": {
                     "200": {
                         "description": "OK",
                         "schema": {
-                            "type": "string"
+                            "$ref": "#/definitions/v1.registerResponse"
+                        }
+                    },
+                    "400": {
+                        "description": "Bad Request",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrResponse"
+                        }
+                    },
+                    "422": {
+                        "description": "Unprocessable Entity",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrResponse"
+                        }
+                    },
+                    "500": {
+                        "description": "Internal Server Error",
+                        "schema": {
+                            "$ref": "#/definitions/response.ErrResponse"
                         }
                     }
+                }
+            }
+        }
+    },
+    "definitions": {
+        "response.ErrResponse": {
+            "type": "object",
+            "properties": {
+                "error": {
+                    "type": "string"
+                }
+            }
+        },
+        "v1.registerRequest": {
+            "type": "object",
+            "required": [
+                "email",
+                "password"
+            ],
+            "properties": {
+                "email": {
+                    "type": "string",
+                    "example": "email@example.com"
+                },
+                "password": {
+                    "type": "string",
+                    "example": "y0urP@ssw0rd"
+                }
+            }
+        },
+        "v1.registerResponse": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "string",
+                    "example": "d13a75e2-3d21-4e57-9dc0-3a7f5bee4c25"
                 }
             }
         }
@@ -47,8 +113,8 @@ var SwaggerInfo = &swag.Spec{
 	Host:             "localhost:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
-	Title:            "Go API",
-	Description:      "...",
+	Title:            "Universal authorization service API",
+	Description:      "Authorization, registration, etc...",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
